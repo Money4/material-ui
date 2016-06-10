86d6dc49fcbabf28f90a85286f5afdc806f6177c
Igor Line <igor@line.ee>
2016-06-10 11:05:03 +0200

    added styling to nestedlist

--- src/List/NestedList.js
+++ src/List/NestedList.js
@@ -1,5 +1,41 @@
 import React, {Component, PropTypes} from 'react';
+import {fade} from '../utils/colorManipulator';
 import List from './List';
+
+function getStyles(props, context, state) {
+  const {
+    open
+  } = props;
+
+  const {muiTheme} = context
+  const {nestedList} = muiTheme
+
+  const textColor = muiTheme.baseTheme.palette.textColor
+  const backgroundColor = fade(textColor, 0.1)
+  // const singleAvatar = !secondaryText && (leftAvatar || rightAvatar);
+  // const singleNoAvatar = !secondaryText && !(leftAvatar || rightAvatar);
+  // const twoLine = secondaryText && secondaryTextLines === 1;
+  // const threeLine = secondaryText && secondaryTextLines > 1;
+
+  const styles = {
+    root: {
+      // backgroundColor: (state.isKeyboardFocused || state.hovered) &&
+      // !state.rightIconButtonHovered &&
+      // !state.rightIconButtonKeyboardFocused ? hoverColor : null,
+      // color: textColor,
+
+      backgroundColor,
+      padding: nestedList.padding,
+      display: open ? null : 'none',
+      // fontSize: 16,
+      // marginLeft: '16px',
+      // position: 'relative',
+    },
+
+  };
+
+  return styles;
+}
 
 class NestedList extends Component {
   static propTypes = {
@@ -9,7 +45,7 @@ class NestedList extends Component {
     /**
      * Override the inline-styles of the root element.
      */
-    style: PropTypes.object,
+    style: PropTypes.object
   };
 
   static defaultProps = {
@@ -17,19 +53,25 @@ class NestedList extends Component {
     open: false,
   };
 
+  static contextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+  };
+
   render() {
     const {
       children,
-      open,
+      // open,
       nestedLevel,
       style,
     } = this.props;
 
-    const styles = {
-      root: {
-        display: open ? null : 'none',
-      },
-    };
+    // const styles = {
+      // root: {
+        // display: open ? null : 'none',
+      // },
+    // };
+    //
+    const styles = getStyles(this.props, this.context, this.state)
 
     return (
       <List style={Object.assign({}, styles.root, style)}>
@@ -37,7 +79,7 @@ class NestedList extends Component {
           React.Children.map(children, (child) => {
             return React.isValidElement(child) ? (
               React.cloneElement(child, {
-                nestedLevel: nestedLevel + 1,
+                nestedLevel: nestedLevel + 1
               })
             ) : child;
           })
